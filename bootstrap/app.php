afc099548c15fbd0c7a5f295802e7e3e7a28e84e
qingxiangguoji-travel <qingxiangguoji@gmail.com>
2026-01-21 16:09:12 +0700

    Fix Laravel 12 bootstrap compatibility

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,12 +3,11 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
@@ -19,9 +18,4 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })
-    ->create()
-    ->useRequest(Request::capture())
-    ->trustProxies(
-        at: '*',
-        headers: Request::HEADER_X_FORWARDED_ALL
-    );
+    ->create();
